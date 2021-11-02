4cc753af05948dbac68e859b20dd8b336a5aa350
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-11-02 12:31:15 -0700

    router fleshed out

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,19 @@
-// DO YOUR MAGIC
+const express = require('express');
+//knex
+//db
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json({ message: 'returns an array of cars sorted by id (or an empty array if there are not any)' })
+})
+
+router.get('/:id', (req, res) => {
+    res.json({ message: 'returns the car by given id' })
+})
+
+router.post('/', (req, res) => {
+    res.json({ message: 'returns the created car' })
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,8 +1,14 @@
 const express = require("express")
 
+const carsRouter = require('./cars/cars-router.js')
+
 const server = express()
 
-server.get('/', (req, res) => {
+server.use(express.json());
+
+server.use('/api/cars', carsRouter)
+
+server.get('*', (req, res) => {
     res.json({ message: 'server is up!' })
 })
 
